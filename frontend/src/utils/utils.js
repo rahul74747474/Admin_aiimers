b0f26dcb995748dbf93a82c6bbc2c566110bfc18
rahul74747474 <ghibli141525@gmail.com>
2025-08-04 18:12:16 +0530

    Update utils.js

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:5000/api/v1";
+export const BACKEND_URL = "https://admin-aiimers.onrender.com/api/v1";
 // export const BACKEND_URL = "http://localhost:3000/api/v1";
